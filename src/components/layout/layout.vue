ad90635c9f3ff0c90cea3d6ae098a1b0576b05db
peien <peien@iyenei.com>
2018-08-15 17:28:43 +0800

    layout

---
+++ src/components/layout/layout.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="layout" :class="{'hasSlider': layoutClass.hasSlider}">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'x-layout',
+        data() {
+            return {
+                layoutClass: {
+                    hasSlider: false
+                }
+            }
+        },
+        mounted () {
+            console.log(this.$children)
+            this.$children.forEach(item => {
+                console.log(item.$options.name)
+                if(item.$options.name =='x-sider') {
+                    this.layoutClass.hasSlider = true 
+                }
+            });
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+.layout{
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1; 
+    height: 100vh;
+    &.hasSlider{
+        flex-direction: row;
+    }
+}
+</style>
---
+++ src/components/layout/sider.vue
@@ -0,0 +1,15 @@
+ <template>
+    <div class="slider">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'x-sider'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style> 
