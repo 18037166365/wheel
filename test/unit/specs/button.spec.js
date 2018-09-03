14cbf0f5bf3797536c8690b870a898b1a607e715
peien <peien@iyenei.com>
2018-09-03 15:55:56 +0800

    test

--- test/unit/specs/button.spec.js
+++ test/unit/specs/button.spec.js
@@ -1,22 +1,58 @@
-import { expect } from 'chai'
-import { shallowMount, mount } from '@vue/test-utils'
+import chai, {expect} from 'chai'
+import sinon from 'sinon'
+import sinonChai from 'sinon-chai'
+import {shallowMount, mount} from '@vue/test-utils'
 import Button from '@/components/button/button';
+chai.use(sinonChai)
 
-
-describe('Button', () => {
-
-  it('存在', ()=> {
-    const wrapper = shallowMount(Button)
+describe('Button.vue', () => {
+  it('存在.', () => {
     expect(Button).to.exist
   })
+  it('可以设置icon.', () => {
+    const wrapper = mount(Button, {
+      propsData: {
+        icon: 'settings'
+      }
+    })
+    const useElement = wrapper.find('use')
+    expect(useElement.attributes()['href']).to.equal('#i-settings')
+  })
+  it('可以设置loading.', () => {
+    const wrapper = mount(Button, {
+      propsData: {
+        icon: 'settings',
+        loading: true
+      }
+    })
+    const vm = wrapper.vm
+    const useElements = vm.$el.querySelectorAll('use')
+    expect(useElements.length).to.equal(1)
+    expect(useElements[0].getAttribute('xlink:href')).to.equal('#i-loading1')
+  })
 
-  // it('可以设置icon', ()=> {
-  //   const wrapper = mount(Button, {
-  //     propsData: {
-  //       icon: 'loading'
-  //     }
-  //   })
-  //   const useElement = wrapper.find('use')
-  //   expect(useElement.attributes()['href']).to.equal('#i-loading')
-  // })
+  it('icon 默认的 order 是 1', () => {
+    const wrapper = mount(Button, {
+      attachToDocument: true,
+      propsData: {
+        icon: 'settings',
+      }
+    })
+    const vm = wrapper.vm
+    const icon = vm.$el.querySelector('svg')
+    expect(getComputedStyle(icon).order).to.eq('1')
+  })
+
+  it('设置 iconPosition 可以改变 order', () => {
+    const wrapper = mount(Button, {
+      attachToDocument: true,
+      propsData: {
+        icon: 'settings',
+        iconPosition: 'right'
+      }
+    })
+    const vm = wrapper.vm
+    const icon = vm.$el.querySelector('svg')
+    expect(getComputedStyle(icon).order).to.eq('2')
+  })
 })
